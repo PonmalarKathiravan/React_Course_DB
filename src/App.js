16354efdaef1db567830e9b54eba76fff11fc827
PonmalarKathiravan <Ponmalar@iamneo.ai>
2023-01-03 05:52:25 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <header>
+    <h1>Let we see the output of JAVASCRIPT</h1>  
+    </header>
+
+<button onClick={window['alertMsg']}>JS Call</button>
+<h3>have you checked the developer message?</h3>
+  </div>
+  );
+}
+
+export default App;
